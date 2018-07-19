eafe95cbbf4a5d728ba3a2f5954493c118e842aa
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-07-19 03:18:11 -0600

    withdrawals

--- index.js
+++ index.js
@@ -13,15 +13,20 @@
 
 var customer = "";
 var _balance = 100;
+var withdrawTotal = 20;
 
 var withdrawFees = 2.50;
+
 var balance = document.getElementById("balance");
+var _withdraw =
 
-function withdraw (amount) {
-    return balance - amount;
-}
+ function withdraw(){
+     _balance = _balance - withdrawFees - withdrawTotal;
+    return balance.innerHTML = _balance; 
+};
 
-window.onload = function() {
+
+window.onload = function() {            // load up balance in table
     balance.innerHTML = _balance;   
 };
 
