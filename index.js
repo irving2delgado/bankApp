a4b61e971a3734f10f3cc59cc4bd947254954832
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-07-19 03:05:04 -0600

    add variables

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-customer
-bankAccount
-balance
-fees 
-withdrawal 
-transaction 
-Interest 
-interestRate
+// customer
+// bankAccount
+// balance
+// fees 
+// withdrawal 
+// transaction 
+// Interest 
+// interestRate
 
-deposit
-withdraw
-getInterest
+// deposit
+// withdraw
+// getInterest
+
+var customer = "";
+var _balance = 100;
+
+var withdrawFees = 2.50;
+var balance = document.getElementById("balance");
+
+function withdraw (amount) {
+    return balance - amount;
+}
+
+window.onload = function() {
+    balance.innerHTML = _balance;   
+};
+
+
